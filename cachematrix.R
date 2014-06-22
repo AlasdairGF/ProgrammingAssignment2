d052b9f6f4bfe92004403042458de5866dd08a1e
AlasdairGF <alasdair@gordon-finlayson.net>
2014-06-22 23:47:33 +0100

    Magic box stage - not coded actual inversion

--- cachematrix.R
+++ cachematrix.R
@@ -4,11 +4,30 @@
 ## Consists of 2 functions - one for getting & setting the cached matrix,
 ## the other for performing the actual matrix inversion.
 ##
+## Probably too many comments...!
+##
 
 ## makeCacheMatrix
-## This creates the matrix object to be inverted
+## This returns 4 functions as a list which will set up a matrix object,
+## report its value, store the inverse (calculated in second fn) and also
+## report the inverted value
 
 makeCacheMatrix <- function(x = matrix()) {
+      
+      cachedMatrix <- NULL
+      
+      set <- function (incomingMatrix) {
+            # assign incoming matrix to x which exists in scope of makeCacheMatrix
+            # not in the set function (hence the <<- bit)
+            x <<- incomingMatrix 
+            cachedMatrix <<- NULL # clear matrix as re-calc will be needed
+      }
+      get <- function() x # just return the matrix
+      setMatrix <- function(invertedMatrix) cachedMatrix <<- invertedMatrix
+      getMatrix <- function() cachedMatrix
+      
+      # makeCacheMatrix fn returns the list of 4 functions:
+      list(set = set, get = get, setMatrix = setMatrix, getMatrix = getMatrix)
 
 }
 
@@ -18,5 +37,21 @@ makeCacheMatrix <- function(x = matrix()) {
 ## if not, then solve & cache solution.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      inversion <- x$getMatrix()
+      if (!is.null(inversion)) {
+            ## so if inversion has already been calculated, don't re-do it
+            ## just return the value already cached in the variable
+            message("Returning cached inversion...")
+            return(inversion)
+      }
+      workingMatrix <- x$get() # local variable to play with
+      
+      message ("Calculating inversion...")
+
+      ## magic box where we calculate inverted matrix
+      ## stored in local variable 'inversion'
+      
+      x$setMatrix(inversion) # cache the solution for next time
+      inversion # return the solution
 }
